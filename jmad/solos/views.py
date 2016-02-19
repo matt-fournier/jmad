c7ca051d409d6898efe91b7f1cb6a6708264f0d4
Matt Fournier <matt.fournier.dv@gmail.com>
2016-02-18 21:46:00 -0500

    initial index template, view test

--- jmad/solos/views.py
+++ jmad/solos/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
 
-def index():
-    pass
+def index(request):
+    return render_to_response('solos/index.html')
